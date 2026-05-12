3c5a19e28a604e554482be22f48e349973ce9e0b
Saatvik Kumar <saatvikkumar39@gmail.com>
2026-05-12 12:36:00 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        if(head==nullptr) return head;
+        ListNode* next=head->next;
+        while(curr!=nullptr){
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            if(next!=nullptr)next=next->next;
+        }
+        return prev;
+    }
+};
